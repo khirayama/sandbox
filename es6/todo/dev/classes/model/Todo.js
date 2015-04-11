c47d35fe531f13b6ce30fa25f3c7e46536defc7b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-04-12 08:51:35 +0900

    Modelクラスを作成

--- es6/todo/dev/classes/model/Todo.js
+++ es6/todo/dev/classes/model/Todo.js
@@ -1,42 +1,29 @@
 'use strict';
 import 'babel/polyfill';
-import {EventEmitter} from 'events';
-import AppDispatcher from '../dispatcher/AppDispatcher';
+import Model from './Model';
 
-const CHANGE_EVENT = 'CHANGE';
-
-class Todo extends EventEmitter {
+class Todo extends Model {
   constructor() {
-    super();
-    this._todos = {};
-
-    AppDispatcher.register((action) => {
-      this._onAction(action);
-    });
-  }
-  _onAction(action) {
-    switch(action.actionType) {
-      case 'TODO_CREATE':
+    let actions = {
+      'TODO_CREATE': (action) => {
         let text = action.text.trim();
         if (text !== '') {
           this._create(text);
-          this.emitChange();
         }
-        break;
-      case 'TODO_UNDO_COMPLETE':
+      },
+      'TODO_UNDO_COMPLETE': (action) => {
         this._update(action.id, {complete: false});
-        this.emitChange();
-        break;
-      case 'TODO_COMPLETE':
+      },
+      'TODO_COMPLETE': (action) => {
         this._update(action.id, {complete: true});
-        this.emitChange();
-        break;
-      case 'TODO_DESTROY':
+      },
+      'TODO_DESTROY': (action) => {
         this._destroy(action.id);
-        this.emitChange();
-        break;
-      default:
-    }
+      }
+    };
+    super(actions);
+
+    this._todos = {};
   }
   _create(text) {
     let id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
@@ -55,15 +42,6 @@ class Todo extends EventEmitter {
 
   getAll() {
     return this._todos;
-  }
-  emitChange() {
-    this.emit(CHANGE_EVENT);
-  }
-  addChangeListener(callback) {
-    this.on(CHANGE_EVENT, callback);
-  }
-  removeChangeListener(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
   }
 }
 export default new Todo();
