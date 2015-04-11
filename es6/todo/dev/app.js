24ef26f5375a78e365ef717b1ea567b4cc214b6c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-04-12 00:06:42 +0900

    dispatcher削ってる。興味だけ

--- es6/todo/dev/app.js
+++ es6/todo/dev/app.js
@@ -2,6 +2,7 @@ import Todo from './classes/model/Todo';
 import AppDispatcher from './classes/dispatcher/AppDispatcher';
 
 var todo = Todo.getAll();
+var keys, id;
 
 console.log(`nothing: ${JSON.stringify(todo)}`);
 
@@ -9,8 +10,34 @@ AppDispatcher.dispatch({
   actionType: 'TODO_CREATE',
   text: 'aaaa'
 });
-var keys = Object.keys(todo);
-var id = keys[0];
+keys = Object.keys(todo);
+id = keys[0];
+console.log(`create: ${JSON.stringify(todo)}`);
+
+AppDispatcher.dispatch({
+  actionType: 'TODO_COMPLETE',
+  id: id
+});
+console.log(`toggle to true: ${JSON.stringify(todo)}`);
+
+AppDispatcher.dispatch({
+  actionType: 'TODO_UNDO_COMPLETE',
+  id: id
+});
+console.log(`toggle to false: ${JSON.stringify(todo)}`);
+
+AppDispatcher.dispatch({
+  actionType: 'TODO_DESTROY',
+  id: id
+});
+console.log(`destory: ${JSON.stringify(todo)}`);
+
+AppDispatcher.dispatch({
+  actionType: 'TODO_CREATE',
+  text: 'nnnn'
+});
+keys = Object.keys(todo);
+id = keys[0];
 console.log(`create: ${JSON.stringify(todo)}`);
 
 AppDispatcher.dispatch({
--- es6/todo/dev/classes/dispatcher/Dispatcher.js
+++ es6/todo/dev/classes/dispatcher/Dispatcher.js
@@ -1,63 +1,43 @@
 'use strict';
-var _lastID = 1;
-var _prefix = 'ID_';
 
 function Dispatcher() {
-  this.$Dispatcher_callbacks = {};
-  this.$Dispatcher_isPending = {};
-  this.$Dispatcher_isHandled = {};
-  this.$Dispatcher_isDispatching = false;
-  this.$Dispatcher_pendingPayload = null;
+  this._lastID = 1;
+  this.callbacks = {};
+  this.isPending = {};
+  this.pendingPayload = null;
 }
 Dispatcher.prototype.register=function(callback) {
-  var id = _prefix + _lastID++;
-  this.$Dispatcher_callbacks[id] = callback;
+  var id = 'ID_' + this._lastID++;
+  this.callbacks[id] = callback;
   return id;
 };
 Dispatcher.prototype.unregister=function(id) {
-  delete this.$Dispatcher_callbacks[id];
-};
-Dispatcher.prototype.waitFor=function(ids) {
-  for (var ii = 0; ii < ids.length; ii++) {
-    var id = ids[ii];
-    if (this.$Dispatcher_isPending[id]) {
-      continue;
-    }
-    this.$Dispatcher_invokeCallback(id);
-  }
+  delete this.callbacks[id];
 };
 Dispatcher.prototype.dispatch=function(payload) {
-  this.$Dispatcher_startDispatching(payload);
+  this.startDispatching(payload);
   try {
-    for (var id in this.$Dispatcher_callbacks) {
-      if (this.$Dispatcher_isPending[id]) {
+    for (var id in this.callbacks) {
+      if (this.isPending[id]) {
         continue;
       }
-      this.$Dispatcher_invokeCallback(id);
+      this.invokeCallback(id);
     }
   } finally {
-    this.$Dispatcher_stopDispatching();
+    this.stopDispatching();
   }
 };
-Dispatcher.prototype.isDispatching=function() {
-  return this.$Dispatcher_isDispatching;
+Dispatcher.prototype.invokeCallback=function(id) {
+  this.isPending[id] = true;
+  this.callbacks[id](this.pendingPayload);
 };
-Dispatcher.prototype.$Dispatcher_invokeCallback=function(id) {
-  this.$Dispatcher_isPending[id] = true;
-  this.$Dispatcher_callbacks[id](this.$Dispatcher_pendingPayload);
-  this.$Dispatcher_isHandled[id] = true;
-};
-Dispatcher.prototype.$Dispatcher_startDispatching=function(payload) {
-  for (var id in this.$Dispatcher_callbacks) {
-    this.$Dispatcher_isPending[id] = false;
-    this.$Dispatcher_isHandled[id] = false;
+Dispatcher.prototype.startDispatching=function(payload) {
+  for (var id in this.callbacks) {
+    this.isPending[id] = false;
   }
-  this.$Dispatcher_pendingPayload = payload;
-  this.$Dispatcher_isDispatching = true;
+  this.pendingPayload = payload;
 };
-Dispatcher.prototype.$Dispatcher_stopDispatching=function() {
-  this.$Dispatcher_pendingPayload = null;
-  this.$Dispatcher_isDispatching = false;
+Dispatcher.prototype.stopDispatching=function() {
+  this.pendingPayload = null;
 };
-
 export default Dispatcher;
