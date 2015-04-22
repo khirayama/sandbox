c94ca04da5268545776170d1d19f58dbdde9aa41
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-04-23 07:41:06 +0900

    [wip]templateの関数化

--- es6/todo/dev/classes/viewController/TodoItem.js
+++ es6/todo/dev/classes/viewController/TodoItem.js
@@ -7,7 +7,7 @@ export default class TodoItem {
       isEditing: false
     };
     this.todo = todo;
-    this.template =
+    this.template = this._template
       `<li class="${this._cx({
         'completed': this.todo.complete,
         'edit': this.state.isEditing
@@ -19,7 +19,7 @@ export default class TodoItem {
           <div class="destroy"></div>
         </div>
       </li>`;
-    this.el = this._createElements(this.template);
+    this.el = this._createElements(this.template());
     this.events();
   }
   setState(state) {
@@ -38,21 +38,9 @@ export default class TodoItem {
     });
   }
   _update() {
-    this.template =
-      `<li class="${this._cx({
-        'completed': this.todo.complete,
-        'edit': this.state.isEditing
-      })}">
-        <div class="view">
-          <div class="toggle"></div>
-          <label>${this.todo.text}</label>
-          <input value="${this.todo.text}"></input>
-          <div class="destroy"></div>
-        </div>
-      </li>`;
-
+    console.log(this.template, this._cx({ edit: this.state.isEditing  }));
     let parentNode = this.el.parentNode;
-    let tmp = this._createElements(this.template);
+    let tmp = this._createElements(this.template());
     parentNode.replaceChild(tmp, this.el);
     this.el = tmp;
     this.events();
@@ -65,6 +53,20 @@ export default class TodoItem {
   _onClick() {
     this.setState({isEditing: true});
   }
+  _template() {
+    let args = arguments;
+    let args2 = Array.from(arguments).slice(1);
+    console.log(args, args2);
+    return function() {
+      console.log(args, this, String.raw.apply(null, args));
+      let i, _template = '';
+      for(i = 1; i < args.length; i++) {
+        _template += args[0][i - 1] + args[i];
+      }
+      _template += args[0][i - 1];
+      return _template;
+    };
+  }
   _cx(classNames) {
     let classStr = [];
     for(let className in classNames) {
@@ -75,3 +77,21 @@ export default class TodoItem {
     return classStr.join(' ');
   }
 }
+
+// ちょっと試してる
+function _template() {
+  let template = '';
+  let i;
+  for(i = 1; i < arguments.length; i++) {
+    template += arguments[0][i - 1] + arguments[i];
+  }
+  template += arguments[0][i - 1];
+  return template;
+}
+var todo = {
+  text: 'todo text',
+  complete: false
+};
+let template = _template`text: ${todo.text} / complete: ${todo.complete} / ok`;
+console.log(template);
+
