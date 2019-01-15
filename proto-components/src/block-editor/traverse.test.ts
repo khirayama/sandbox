f1c7be6c35cd92f1cafa676f6de6257324b0f7a4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-01-16 06:30:49 +0900

    Install jest

---
+++ proto-components/src/block-editor/traverse.test.ts
@@ -0,0 +1,77 @@
+import * as uuid from 'uuid/v4';
+
+import { Clap } from './traverse';
+
+describe('Clap', () => {
+  /* ---------------- Node ------------------ */
+  describe('Node', () => {
+    it('Create Node instance without pureNode', () => {
+      const node: Clap.Node = new Clap.Node();
+      expect(node.id).not.toBeFalsy();
+      expect(node.text).toEqual('');
+      expect(node.type).toBeNull();
+      expect(node.nodes.length).toBe(0);
+    });
+    it('Create Node instance with pureNode', () => {
+      const pureNode: Clap.INode = {
+        id: uuid(),
+        text: 'Hello World',
+        type: null,
+        properties: null,
+        nodes: [],
+      };
+      const node: Clap.Node = new Clap.Node(pureNode);
+      expect(node.id).toBe(pureNode.id);
+      expect(node.text).toEqual('Hello World');
+      expect(node.type).toBeNull();
+      expect(node.properties).toBeNull();
+      expect(node.nodes.length).toBe(0);
+    });
+    it('Create Node instance with pureNode has child nodes', () => {
+      const pureNode: Clap.INode = {
+        id: uuid(),
+        text: 'Hello',
+        type: null,
+        properties: null,
+        nodes: [{
+          id: uuid(),
+          text: 'World',
+          type: null,
+          properties: null,
+          nodes: [],
+        }],
+      };
+      const node: Clap.Node = new Clap.Node(pureNode);
+      expect(node.id).toBe(pureNode.id);
+      expect(node.text).toEqual('Hello');
+      expect(node.type).toBeNull();
+      expect(node.properties).toBeNull();
+      expect(node.nodes.length).toBe(1);
+      expect(node.nodes[0].id).toBe(pureNode.nodes[0].id);
+      expect(node.nodes[0].text).toEqual('World');
+      expect(node.nodes[0].type).toBeNull();
+      expect(node.nodes[0].properties).toBeNull();
+      expect(node.nodes[0].nodes.length).toBe(0);
+    });
+    describe('toPureNode', () => {
+      it('Create Node instance with pureNode has child nodes', () => {
+        const pureNode: Clap.INode = {
+          id: uuid(),
+          text: 'Hello',
+          type: null,
+          properties: null,
+          nodes: [{
+            id: uuid(),
+            text: 'World',
+            type: null,
+            properties: null,
+            nodes: [],
+          }],
+        };
+        const node: Clap.Node = new Clap.Node(pureNode);
+        const newPureNode: Clap.INode = node.toPureNode();
+        expect(newPureNode).toEqual(pureNode);
+      });
+    });
+  });
+});
