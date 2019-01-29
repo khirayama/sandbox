a6f2835492a9a6dd3eefb75df6ab563434d92301
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-01-30 08:33:09 +0900

    Create firebase-app

---
+++ firebase-app/.storybook/webpack.config.js
@@ -0,0 +1,35 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json',
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.json',
+          },
+        },
+      },
+    ],
+  },
+};
---
+++ firebase-app/src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export class App extends React.Component<{}, {}> {
+  public render(): JSX.Element {
+
+    return (
+      <div>Hello</div>
+    );
+  }
+}
