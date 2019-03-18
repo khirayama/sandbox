ec9ede724fa62395c83708d50fa64f80c0fc1bb9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-03-19 06:29:32 +0900

    Update crawlar entry point

--- coffee-crawler/src/index.ts
+++ coffee-crawler/src/index.ts
@@ -1,63 +1,30 @@
-import * as puppeteer from 'puppeteer';
+import { Crawler } from './Crawler';
 
-/* Request */
-interface IRequest {
-  url: string;
-  isMobile: boolean;
-}
-
-/* Page */
-interface IAlternate {
-  url: string;
-  lang: string;
-}
-
-interface ILocation {
-  hash: string;
-  host: string;
-  hostname: string;
-  href: string;
-  origin: string;
-  pathname: string;
-  port: string;
-  protocol: string;
-  search: string;
-}
-
-interface IHead {
-  title: string;
-  description: string;
-  keywords: string;
-  // FYI: og:image, twitter:image and so on
-  images: string[];
-  canonical: string;
-  alternates: IAlternate[];
-}
-
-interface IBody {
-  html: string;
-}
-
-interface IPage {
-  head: IHead;
-  body: IBody;
-  location: ILocation;
-}
-
-/* Result */
-interface ILink {
-  isExternal: boolean;
-  text: string;
-  url: string;
-}
-
-interface IResult {
-  text: string;
-  links: ILink[];
-}
 
 /*
- * ページとページの関係、もしくはサイトという関係性をどう示し構築するか
+ * ページとページの関係、もしくはサイトという関係性をどう示し構築するか。
+ * それはそもそも別か。となると以下のフローでよい？
+ * hostnameが一致するものを内部リンク。それ以外を外部リンク。と定義。
+ * - entry pointとなるurlを決める
+ * - そのhostnameに紐づけて、管理用内部リクエストキュー、管理用外部リクエストキューを作成
+ * - PageView取得
+ * - 内部リンクを管理用内部リンクリストに追加(パス解決済)
+ * - 外部リンクを管理用外部リンクリストに追加
+ * - 内部リンクをクロール
+ * - すでにcrawlされており10日以内のリンクはスキップ
+ * - 内部リンクリクエストキューが消化されたら外部へ
+ *
+ * facebook、twitter、instagram、google mapsなどの内部リンクは厳しい気がする
+ * 内部が巨大ネットワークになっているタイプは厳しいのか
  */
 
-console.log('Hi');
+console.log('Launch a crawler.');
+
+(async () => {
+  const entryUrl: string = 'https://www.saredocoffee.com/';
+  // const entryUrl: string = 'https://www.facebook.com/nocoffeefukjapan/';
+  console.log(`entry url: ${entryUrl}`);
+
+  const crawler: Crawler = new Crawler();
+  await crawler.start(entryUrl, false);
+})();
