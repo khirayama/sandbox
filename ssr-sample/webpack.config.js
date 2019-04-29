77d8eac8c23c73214f2245e755f0618b837b0154
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-04-29 17:04:58 +0900

    Install webpack

---
+++ ssr-sample/webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = (env, argv) => {
+  const config = {
+    entry: {
+      index: path.resolve('src', 'client', 'index.tsx'),
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve('dist', 'public'),
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: './tsconfig.client.json',
+        }),
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify({
+          NODE_ENV: process.env.NODE_ENV,
+        }),
+      }),
+    ],
+    optimization: {
+      minimize: argv.mode === 'production',
+      // splitChunks: {
+      //   name: 'dist/public/commons/index',
+      //   chunks: 'initial',
+      //   cacheGroups: {
+      //     vendors: {
+      //       test: /node_modules/,
+      //       name: 'dist/public/commons/vendors',
+      //       chunks: 'initial'
+      //     },
+      //   },
+      // },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.client.json',
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  if (NODE_ENV === 'production') {
+    console.log(`Building as production...`);
+    config.optimization.minimize = true;
+
+    if (process.env.ANALYSIS === 'true') {
+      console.log(`Building for analyze...`);
+      config.plugins.push(new BundleAnalyzerPlugin());
+    }
+  } else {
+    console.log('Building as development...');
+    config.devtool = 'inline-source-map';
+  }
+  return config;
+};
