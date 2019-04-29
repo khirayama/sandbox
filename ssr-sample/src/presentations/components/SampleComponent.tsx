d034b4323508127a7f70e2138ea7aa030de5f848
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-04-30 08:00:08 +0900

    Use styled-components

--- ssr-sample/src/presentations/components/SampleComponent.tsx
+++ ssr-sample/src/presentations/components/SampleComponent.tsx
@@ -1,30 +1,44 @@
 import * as React from 'react';
 import { Route, Link } from 'react-router-dom';
 import loadable from '@loadable/component';
+import * as styled from 'styled-components';
 
 const LoadableIndex = loadable((): any => import(/* webpackChunkName: "home" */'presentations/components/Home').then(({ Home }) => Home));
 const LoadableAbout = loadable((): any => import(/* webpackChunkName: "about" */'presentations/components/About').then(({ About }) => About));
 const LoadableUsers = loadable((): any => import(/* webpackChunkName: "users" */'presentations/components/Users').then(({ Users }) => Users));
 
+const GlobalStyle = styled.createGlobalStyle`
+  body {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+`;
+
 export function SampleComponent() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-          <li>
-            <Link to="/users/">Users</Link>
-          </li>
-        </ul>
-      </nav>
-      <Route exact path="/" component={LoadableIndex} />
-      <Route exact path="/about/" component={LoadableAbout} />
-      <Route exact path="/users/" component={LoadableUsers} />
-    </div>
+    <>
+      <GlobalStyle />
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={LoadableIndex} />
+        <Route exact path="/about/" component={LoadableAbout} />
+        <Route exact path="/users/" component={LoadableUsers} />
+      </div>
+    </>
   );
 }
