d1bc5a23c4e1bed7ea9b63cd82fd2a5b01159951
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-04-29 23:27:30 +0900

    WIP

--- ssr-sample/src/server/renderer.tsx
+++ ssr-sample/src/server/renderer.tsx
@@ -1,10 +1,16 @@
+import * as path from 'path';
+
 import express from 'express';
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import { ChunkExtractor } from '@loadable/server';
 
 import { renderFullPage } from 'server/renderFullPage';
 import { SampleComponent } from 'presentations/components/SampleComponent';
+
+const statsFile = path.resolve('dist', 'loadable-stats.json');
+const extractor = new ChunkExtractor({ statsFile });
 
 export function get(req: express.Request, res: express.Response) {
   const context = {};
@@ -12,12 +18,14 @@ export function get(req: express.Request, res: express.Response) {
   const meta = '';
   const assets = [''];
   const body = ReactDOMServer.renderToString(
-    <StaticRouter
-      location={req.url}
-      context={context}
-    >
-      <SampleComponent />
-    </StaticRouter>
+    extractor.collectChunks(
+      <StaticRouter
+        location={req.url}
+        context={context}
+      >
+        <SampleComponent />
+      </StaticRouter>
+    )
   );
   const style = '<style>* { background: gray; }</style>';
   const scripts = `<script src="/public/index.bundle.js"></script>`;
