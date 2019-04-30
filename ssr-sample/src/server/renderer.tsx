e08f82f607ef2a7364f3673bcf9f6a6e3a7a4de2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-04-30 12:27:08 +0900

    Load stylesheet for ssr

--- ssr-sample/src/server/renderer.tsx
+++ ssr-sample/src/server/renderer.tsx
@@ -4,6 +4,7 @@ import express from 'express';
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import * as styled from 'styled-components';
 
 import { renderFullPage } from 'server/renderFullPage';
 import { SampleComponent } from 'presentations/components/SampleComponent';
@@ -11,17 +12,21 @@ import { SampleComponent } from 'presentations/components/SampleComponent';
 export function get(req: express.Request, res: express.Response) {
   const context = {};
 
+  const sheet = new styled.ServerStyleSheet();
+
   const meta = '';
   const assets = [''];
   const body = ReactDOMServer.renderToString(
-    <StaticRouter
-      location={req.url}
-      context={context}
-    >
-      <SampleComponent />
-    </StaticRouter>
+    sheet.collectStyles(
+      <StaticRouter
+        location={req.url}
+        context={context}
+      >
+        <SampleComponent />
+      </StaticRouter>
+    )
   );
-  const style = '<style>* { background: gray; }</style>';
+  const style = sheet.getStyleTags();
   const scripts = `<script src="/public/index.bundle.js"></script>`;
 
   res.send(renderFullPage({
