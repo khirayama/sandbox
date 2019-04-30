34d3cb3b1545a35b26375737b73ffcb79dc1ee91
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-04-30 18:59:02 +0900

    Support meta tags

--- ssr-sample/src/server/renderer.tsx
+++ ssr-sample/src/server/renderer.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import * as styled from 'styled-components';
+import ReactHelmet from 'react-helmet';
 
 import { renderFullPage } from 'server/renderFullPage';
 import { SampleComponent } from 'presentations/components/SampleComponent';
@@ -14,7 +15,6 @@ export function get(req: express.Request, res: express.Response) {
 
   const sheet = new styled.ServerStyleSheet();
 
-  const meta = '';
   const assets = [''];
   const body = ReactDOMServer.renderToString(
     sheet.collectStyles(
@@ -26,6 +26,12 @@ export function get(req: express.Request, res: express.Response) {
       </StaticRouter>
     )
   );
+  const helmetContent = ReactHelmet.renderStatic();
+  const meta = `
+      ${helmetContent.meta.toString()}
+      ${helmetContent.title.toString()}
+      ${helmetContent.link.toString()}
+    `.trim();
   const style = sheet.getStyleTags();
   const scripts = `<script src="/public/index.bundle.js"></script>`;
 
