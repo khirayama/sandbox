9747814e0cb616210c0bde6c8189c8b6db612da9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-11-15 17:08:58 +0900

    Setup tailwind

---
+++ lightlist-web-poc/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    '@tailwindcss/forms',
+  ],
+}
