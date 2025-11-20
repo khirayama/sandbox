e00b437ab7475a8ce4c64c4fb89c7989e34582ac
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-11-20 21:02:58 +0900

    Update _app

--- lightlist-web-poc/src/pages/_app.tsx
+++ lightlist-web-poc/src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+
+import i18n from "@/lib/i18n";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    i18n;
+  }, []);
+
   return <Component {...pageProps} />;
 }
